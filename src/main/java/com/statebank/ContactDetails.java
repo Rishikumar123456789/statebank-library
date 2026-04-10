3d1c74548dbfa7772f9dbcac43c69620a7a0d1b5
Rishikumar <jaisriandugala207>
2026-04-10 20:42:05 +0530

    recent commit 20

---
+++ src/main/java/com/statebank/ContactDetails.java
@@ -0,0 +1,12 @@
+package com.statebank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ContactDetails {
+  private String mobilenumber;
+}
