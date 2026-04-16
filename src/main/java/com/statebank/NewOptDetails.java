77f16c9a0ae79b6888e58948e921d36e0e71d488
Rishikumar <jaisriandugala207>
2026-04-16 18:30:08 +0530

    25 commit

--- src/main/java/com/statebank/NewOptDetails.java
+++ src/main/java/com/statebank/NewOptDetails.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class OtpDetails {
-   private String otp;
-   private String mobile;
+public class NewOptDetails {
+	private String otp;
+	private String mobile;
 }
